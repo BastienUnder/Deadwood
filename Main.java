318a8abc97cac6cf93ba92e70ee6b0ac159652fa
StephenLoudiana <73310161+StephenLoudiana@users.noreply.github.com>
2020-10-24 12:12:11 -0700

    Basic main file

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main{
+	
+	public static Game game;
+	
+	public Main() {
+		
+		game = new Game();
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		new Main();
+	}
+}
+
+
+
+
+
+
+
+
+
+
