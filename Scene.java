5e430102cf9fdd8526be711cc65b9c8ed0642959
BastienUnder <sebastienfranks@gmail.com>
2020-10-24 11:56:45 -0700

    added basic skeleton of Deadwood class diagram

---
+++ Scene.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class Scene {
+    private String sceneName;
+    private int shotCountersLeft;
+    private LinkedList<Role> sceneRoles;
+    private int budget;
+
+    public void Scene(String sceneName, int shotCountersLeft, LinkedList<Role> sceneRoles, int budget){
+
+    }
+
+    public void decrementShotCounter(){
+
+    }
+
+    public void wrapScene(){
+
+    }
+
+}
